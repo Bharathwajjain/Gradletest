7064e2f6fb151998cb535b888f72fb6eed4895ab
Bharathwajjain <bharathwajjain@gmail.com>
2021-02-04 11:55:28 +0100

    Update TestLink.java

--- src/main/java/io/testproject/generated/tests/webinars/TestLink.java
+++ src/main/java/io/testproject/generated/tests/webinars/TestLink.java
@@ -6,7 +6,7 @@ import testlink.api.java.client.TestLinkAPIResults;
 public class TestLink{
 	
 	
-	public static String DEVKEY="7a5430ff57a5162996a4a2945ac5b798";
+	public static String DEVKEY="58a7453f86c7e93d6cbfbc2916ff85dc";
 	public static String URL="http://hello-pomelo-testlink.westeurope.cloudapp.azure.com/lib/api/xmlrpc/v1/xmlrpc.php";
 	static String testProject				= "Webinars";
 	static String testPlan					= "WebinarsPlan";
